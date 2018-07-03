1ce035e0e4338bde8f83e0c8751a4e3f91a59f6e
MaugouMio <maugoumine@gmail.com>
2018-07-03 10:21:43 +0800

    connect lobby

---
+++ Assets/lobby/customHUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class customHUD : MonoBehaviour {
+	public NetworkManager manager;
+	
+	public void startHost () {
+		manager.StartHost();
+	}
+	
+	public void connectTo () {
+		manager.StartClient();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
